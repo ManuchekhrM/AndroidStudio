4c25fe2382bdd64891b1b0d72c4a0d2b5b2bce34
Melibaev Manuchekhr <melibaev.m.i@gmail.com>
2020-11-22 13:40:56 +0500

    Nadeyus pravilno

---
+++ Zanyatie_17/app/src/main/java/manu/melibaev/zanyatie_17/MainActivity.java
@@ -0,0 +1,31 @@
+package manu.melibaev.zanyatie_17;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tajhoney;
+    Button knopka;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tajhoney = findViewById(R.id.tajhoney);
+        knopka = findViewById(R.id.knopka);
+
+        knopka.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                knopka.setText("tajhoney.tj");
+            }
+        });
+    }
+}
