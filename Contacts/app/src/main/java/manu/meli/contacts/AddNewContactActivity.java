0e50b120abc3d5626044313dd346ed61f94e0c83
Manuchekhr Melibaev <melibaev.m.i@gmail.com>
2020-12-05 11:15:05 +0500

    Contacts 30%

---
+++ Contacts/app/src/main/java/manu/meli/contacts/AddNewContactActivity.java
@@ -0,0 +1,14 @@
+package manu.meli.contacts;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddNewContactActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addnewcontact);
+    }
+}
